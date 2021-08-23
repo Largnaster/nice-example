c90fe839ba65894618054b7e0c6086e2c16bf35d
Largnaster <40041090+Largnaster@users.noreply.github.com>
2021-08-23 18:38:16 -0500

    Aplicacion funcionando sin materialUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // --API DE LOS CHISTED DE CHUCK NORRIS-- https://api.chucknorris.io/jokes/random
+  // --API PARA LAS IMAGENES RANDOM-- https://source.unsplash.com/random
+  // --SERIA UN PLUS UTILIZAR MATERIAL UI-- https://material-ui.com/
+  // DEFINICION DE VARIABLES
+  const [contador, setContador] = useState(0);
+  const [chiste, setChiste] = useState("");
+  const [imagen, setImagen] = useState([]);
+
+  // FUNCIONE
+  const aumentar = () => {
+    setContador(contador + 1);
+  };
+  const reducir = () => {
+    if (contador > 0) {
+      setContador(contador - 1);
+    }
+  };
+  const getChiste = async () => {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const content = await response.json();
+    console.log(content.value);
+    setChiste(content.value);
+  };
+  // const getChiste = () => {
+  //   fetch("https://api.chucknorris.io/jokes/random")
+  //   .then(res => res.json())
+  //   .then((result) => {
+  //     console.log(result.value)
+  //   })
+  // }
+  const getImagen = async () => {
+    const response = await fetch("https://source.unsplash.com/random");
+    console.log(response["url"]);
+    setImagen((imagenes) => [...imagenes, response["url"]]);
+
+    // fetch('https://source.unsplash.com/random')
+    // .then(res => res.json())
+    // .then((content) => {
+    //   console.log(content)
+    // })
+  };
+
+  // RENDER
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Cotador</h1>
+        <div>
+          <button onClick={aumentar}>+</button>
+          <button onClick={reducir}>-</button>
+          <button onClick={() => setContador(0)}>reset</button>
+        </div>
+        <p>{contador}</p>
+      </div>
+      <br />
+      <hr />
+      <div>
+        <h1>Chistes</h1>
+        <button onClick={getChiste}>Chiste</button>
+        <p>{chiste}</p>
+      </div>
+      <br />
+      <hr />
+      <div>
+        <h1>Imagenes</h1>
+        <button onClick={getImagen}>Imagen</button>
+        <br/>
+        {imagen.map((src) => {
+          return (
+            <img
+              id={"ima"}
+              key={src}
+              src={src}
+              alt={"Something or someone"}
+              width="400"
+              height="400"
+            ></img>
+          );
+        })}
+      </div>
     </div>
   );
 }
