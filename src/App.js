bd4fac77c742c55555a0d0eeefcab263079107aa
Largnaster <40041090+Largnaster@users.noreply.github.com>
2021-08-24 00:37:21 -0500

    Aplicacion funcionando con material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import { useState } from "react";
+import { Button } from "@material-ui/core";
+import { ButtonGroup } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import ImageList from "@material-ui/core/ImageList"
+import ImageListItem from "@material-ui/core/ImageListItem"
 import "./App.css";
 
+// ESTILOS PARA LAS IMAGENES 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  imageList: {
+    width: '100%',
+    height: 'auto'
+  }
+}))
+
 function App() {
-  // --API DE LOS CHISTED DE CHUCK NORRIS-- https://api.chucknorris.io/jokes/random
-  // --API PARA LAS IMAGENES RANDOM-- https://source.unsplash.com/random
+  // -- link a github: https://github.com/Largnaster/nice-example
   // --SERIA UN PLUS UTILIZAR MATERIAL UI-- https://material-ui.com/
   // DEFINICION DE VARIABLES
   const [contador, setContador] = useState(0);
   const [chiste, setChiste] = useState("");
   const [imagen, setImagen] = useState([]);
+  // estilos
+  const classes = useStyles();
 
   // FUNCIONE
   const aumentar = () => {
@@ -46,30 +68,41 @@ function App() {
 
   // RENDER
   return (
-    <div className="App">
+    <Container>
       <div>
-        <h1>Cotador</h1>
-        <div>
-          <button onClick={aumentar}>+</button>
-          <button onClick={reducir}>-</button>
-          <button onClick={() => setContador(0)}>reset</button>
-        </div>
+        <h1>Contador</h1>
+        <ButtonGroup color="primary" variant="contained" aria-label="contained primary button group">
+          <Button onClick={aumentar}>
+            +
+          </Button>
+          <Button onClick={reducir}>-</Button>
+          <Button onClick={() => setContador(0)}>reset</Button>
+        </ButtonGroup>
         <p>{contador}</p>
       </div>
       <br />
       <hr />
       <div>
         <h1>Chistes</h1>
-        <button onClick={getChiste}>Chiste</button>
+        <Button variant="contained" onClick={getChiste}>Chiste</Button>
         <p>{chiste}</p>
       </div>
       <br />
       <hr />
       <div>
-        <h1>Imagenes</h1>
-        <button onClick={getImagen}>Imagen</button>
-        <br/>
-        {imagen.map((src) => {
+        <h1>Imágenes</h1>
+        <Button variant="contained" onClick={getImagen}>Imagen</Button>
+        <br />
+        <div className={classes.root}>
+          <ImageList rowHeight={300} className={classes.imageList} cols={4}>
+            {imagen.map((src) => (
+              <ImageListItem key={src} cols={1}>
+                <img src={src} alt={"Una imagen simple"} />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </div>
+        {/* {imagen.map((src) => {
           return (
             <img
               id={"ima"}
@@ -80,9 +113,10 @@ function App() {
               height="400"
             ></img>
           );
-        })}
+        })} */}
+        <br/>
       </div>
-    </div>
+    </Container>
   );
 }
 
